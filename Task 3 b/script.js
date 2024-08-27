8ec415fa9d7dffa6e77d4bcd147b2d69b9d3d2b5
durgeshceps <101636946+durgeshceps@users.noreply.github.com>
2024-08-27 20:59:26 +0530

    Add files via upload

---
+++ Task 3 b/script.js
@@ -0,0 +1,22 @@
+// Image Gallery Enlarge Function
+function enlargeImage(imgElement) {
+    var enlargedImage = document.getElementById("enlargedImage");
+    enlargedImage.src = imgElement.src;
+}
+
+// Slideshow Functionality
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
